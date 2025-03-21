745f48591ec45f0d42ed9c2f4b84bf4cc38126b4
Ayush Pratap <ayushprince110@gmail.com>
2025-03-21 15:29:16 +0530

    Kuchh bhi kr diya

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,15 @@
 // import React from 'react'
 
+import Header from "../other/Header"
+import TaskListNumbers from "../other/TaskListNumbers"
+import TaskList from "../TaskList/TaskList"
+
 function EmployeeDashboard() {
   return (
-    <div>
-      Emp
+    <div className="p-10 bg-[#1c1c1c] h-screen">
+      <Header />
+      <TaskListNumbers />
+      <TaskList />
     </div>
   )
 }
